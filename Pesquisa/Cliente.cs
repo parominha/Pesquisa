bad93d57b5b03a1c683a59d171cea498a1f33f7d
paloma.asantos17 <paloma.asantos17@OSA0557596W10-1>
2022-11-01 21:59:20 -0300

    Adicionar arquivos de projeto.

---
+++ Pesquisa/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using Pesquisa.dao.cliente;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pesquisa
+{
+    public partial class FrmCliente : Form
+    {
+        public FrmCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butPesquisa_Click(object sender, EventArgs e)
+        {
+            Cliente Metodo_Cliente = new Cliente();
+            DataSet DataSetResultadoPesquisa = new DataSet();
+
+            DataSetResultadoPesquisa = Metodo_Cliente.Lista_Cliente(tBoxPesquisa.Text);
+            dgCliente.DataSource = DataSetResultadoPesquisa.Tables[0];
+
+            dgCliente.Refresh();
+        }
+      
+    }
+}
---
+++ Pesquisa/dao/cliente/Cliente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pesquisa.dao.cliente
+{
+    class Cliente
+    {
+        string oChave = "Data Source=10.39.45.44;Initial Catalog=Senac2022;Persist Security Info=True;User ID=Turma2022;Password=Turma2022@2022";
+        public DataSet Lista_Cliente(string p_nome)
+        {
+            DataSet DataSetCliente = new DataSet();
+
+            try
+            {
+                SqlConnection Conexao = new SqlConnection(oChave);
+                Conexao.Open();
+                string wQuery = $"select * from Customer_List where Name like '%{p_nome}%'";
+                SqlDataAdapter Adapter = new SqlDataAdapter(wQuery, Conexao);
+                Adapter.Fill(DataSetCliente);                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return DataSetCliente;
+        }
+
+        public void IncluirCliente(string p_Name, string p_Contact, string p_Address, int p_Credit)
+        {
+            try
+            {
+                SqlConnection Conexao = new SqlConnection(oChave);
+                Conexao.Open();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
